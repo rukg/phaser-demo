e5d01d576361a1880a4299c7befdc65592b89abe
Спирин Владимир <spirinv92@gmail.com>
2022-01-11 21:50:12 +0300

    feat(webpack): webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,75 @@
 const path = require('path');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const package = require('./package.json');
 
 module.exports = {
-    entry: './src/index.ts',
-    mode: 'development',
-    devServer: {
-        static: './dist',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/,
-            },
-            {
-                test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
+  entry: './src/index.ts',
+  devServer: {
+    static: './dist'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+          },
         ],
-    },
-    resolve: {
-        extensions: ['.ts', '.js'],
-    },
-    plugins: [
-        new CopyWebpackPlugin({
-            patterns: [
-                {
-                    from: './src/assets/', to: path.resolve(__dirname, 'dist/assets')
-                }
-            ]
-        })
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+      },
     ],
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'phaser',
+          enforce: true,
+          chunks: 'initial',
+        },
+      },
     },
+  },
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: './src/assets/',
+          to: path.resolve(__dirname, 'dist/assets'),
+        },
+      ],
+    }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+      title: package.description,
+      inject: 'body',
+      hot: true,
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js',
+    clean: true,
+  },
 };
